531cd4eb017d6bb04bf9a3fa94692cd64ab9c79b
arthurf-dev <arthurfreitas@tuta.io>
2025-05-17 14:36:37 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-
+// Exibe uma mensagem motivacional ao clicar no botão
+function mostrarMensagem() {
+  const mensagem = document.getElementById("mensagem");
+  mensagem.textContent = "Não espere o momento perfeito, apenas faça do agora o seu ponto de partida, você consegue! seja forte e corajoso!";
+}
